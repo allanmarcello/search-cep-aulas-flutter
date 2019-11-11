6990d45a58f95d2fcf2812a6023ea224efb17e5b
allanmarcello <43879772+allanmarcello@users.noreply.github.com>
2019-11-11 19:21:50 -0300

    atualização 1.1
    atualização 1.1

--- lib/services/via_cep_service.dart
+++ lib/services/via_cep_service.dart
@@ -3,11 +3,17 @@ import 'package:search_cep/models/result_cep.dart';
 
 class ViaCepService {
   static Future<ResultCep> fetchCep({String cep}) async {
-    final response = await http.get('https://viacep.com.br/ws/$cep/json/');
-    if (response.statusCode == 200) {
-      return ResultCep.fromJson(response.body);
-    } else{
-      return ResultCep.fromJson('{ "cep": "Verifique o CEP digitado!",  "logradouro": "Verifique o CEP digitado!",  "complemento": "Verifique o CEP digitado!",  "bairro": "Verifique o CEP digitado!",  "localidade": "Verifique o CEP digitado!",  "uf": "Verifique o CEP digitado!",  "unidade": "Verifique o CEP digitado!",  "ibge": "Verifique o CEP digitado!",  "gia": "Verifique o CEP digitado!" }');
+    try {
+      final response = await http.get('https://viacep.com.br/ws/$cep/json/');
+      if (response.statusCode == 200) {
+        return ResultCep.fromJson(response.body);
+      } else {
+        return ResultCep.fromJson(
+            '{ "cep": "Verifique o CEP digitado!",  "logradouro": "Verifique o CEP digitado!",  "complemento": "Verifique o CEP digitado!",  "bairro": "Verifique o CEP digitado!",  "localidade": "Verifique o CEP digitado!",  "uf": "Verifique o CEP digitado!",  "unidade": "Verifique o CEP digitado!",  "ibge": "Verifique o CEP digitado!",  "gia": "Verifique o CEP digitado!" }');
+      }
+    } catch (error) {
+      return ResultCep.fromJson(
+          '{ "cep": "Sem internet !",  "logradouro": "Sem internet !",  "complemento": "Sem internet !",  "bairro": "Sem internet !",  "localidade": "Sem internet !",  "uf": "Sem internet !",  "unidade": "Sem internet !",  "ibge": "Sem internet !",  "gia": "Sem internet !" }');
     }
   }
 }
